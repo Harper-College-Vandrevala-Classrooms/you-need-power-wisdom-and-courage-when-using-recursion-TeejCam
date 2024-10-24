757bc236e8dbfd3107c697127a8c3f5fe9b7fe43
TeejCam <tjcamalan@gmail.com>
2024-10-24 11:08:32 -0500

    Added the proper implementations in the main and tests as well.

--- src/main/java/com/csc/TriangleNumberCalculator.java
+++ src/main/java/com/csc/TriangleNumberCalculator.java
@@ -2,4 +2,55 @@ package com.csc;
 
 public class TriangleNumberCalculator {
   // Your code goes here!
+
+  /** Sum from 1 to N
+   * Tn = 1 + 2 + 3 + ... + N - 1 + N
+   * T1 = 1
+   * T2 = 1 + 2 = 3
+   * T3 = 1 + 2 + 3 = 6
+   */
+
+  public int value(int n)
+  {
+    //return nth triangular number
+    if (n == 1) {
+      return 1;
+    }
+    else {
+      return n + value(n - 1);
+    }
+
+  }
+
+  public int add(int n, int m)
+  {
+    //adds the nth and mth triangular numbers
+    return value(n) + value(m);
+
+  }
+
+  public int subtract(int n, int m)
+  {
+    //subtracts the nth and mth triangular numbers
+    return value(n) - value(m);
+  }
+
+
+  public static void main(String[] args)
+  {
+    TriangleNumberCalculator calculator = new TriangleNumberCalculator();
+
+     System.out.println(calculator.value(30000)); //this should print out 1
+     System.out.println(calculator.value(2)); //print out 3
+     System.out.println(calculator.value(4)); //print out 10
+
+     System.out.println(calculator.add(30000, 20000)); //print out 2
+     System.out.println(calculator.add(2, 3)); //print out 9
+     System.out.println(calculator.add(4, 2)); //print out 13
+
+     System.out.println(calculator.subtract(30000, 10000)); //print out 0
+     System.out.println(calculator.subtract(2, 3)); //print out -3
+     System.out.println(calculator.subtract(4, 2)); //print out 7
+  }
+
 }
--- src/test/java/com/csc/TestTriangleNumberCalculator.java
+++ src/test/java/com/csc/TestTriangleNumberCalculator.java
@@ -1,6 +1,8 @@
 package com.csc;
 
 import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 public class TestTriangleNumberCalculator {
 
@@ -11,5 +13,39 @@ public class TestTriangleNumberCalculator {
     calculator = new TriangleNumberCalculator();
   }
 
-  // Your tests go here
+  @Test
+  public void testValueFunction() {
+    assertEquals(1, calculator.value(1));
+  }
+
+  @Test
+  public void testValueForTwo() {
+    assertEquals(3, calculator.value(2));
+  }
+
+  @Test
+  public void testValueForThree() {
+    assertEquals(6, calculator.value(3));
+  }
+
+  @Test
+  public void testAddFunction() {
+    assertEquals(4, calculator.add(1, 2));
+  }
+
+  @Test 
+  public void testAddFunctionTwoAndThree() {
+    assertEquals(9, calculator.add(2, 3));
+  }
+
+  @Test 
+  public void testSubtractFunction() {
+    assertEquals(0, calculator.subtract(1, 1));
+  }
+
+  @Test 
+  public void testSubtractTwoAndThree() {
+    assertEquals(-3, calculator.subtract(2, 3));
+  }
 }
+
